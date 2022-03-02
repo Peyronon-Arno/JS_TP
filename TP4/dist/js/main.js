f18862d15f8d1699850876adeb968e365c5ada8f
Arno Peyronon <84981009+Peyronon-Arno@users.noreply.github.com>
2022-03-02 17:44:40 +0100

    Début du tp4
    Ajout des fichiers pour le tp4

---
+++ TP4/dist/js/main.js
@@ -0,0 +1,23 @@
+Vue.createaApp({
+    data() {
+        return {
+            title: 'TP5 JS'
+        }
+    }
+}).mount(h1);
+
+Vue.createaApp({
+    data() {
+        return {
+            title: 'TP5 JS'
+        }
+    }
+}).mount('#news');
+
+Vue.createaApp({
+    data() {
+        return {
+            title: 'TP5 JS'
+        }
+    }
+}).mount('#click');
