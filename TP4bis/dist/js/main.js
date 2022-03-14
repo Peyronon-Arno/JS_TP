214fbe196bdf2a3c82c8f2293aff4dfac30d63c9
Arno Peyronon <84981009+Peyronon-Arno@users.noreply.github.com>
2022-03-14 16:12:26 +0100

    TP4 bis
    Fin de validation du formulaire
    Manque le reste a partir du composant vue.js

--- TP4bis/dist/js/main.js
+++ TP4bis/dist/js/main.js
@@ -1,34 +1,56 @@
-import ButtonCounter from '/view/components/ButtonCounter.js';
+//remplacer le code ci-dessous par le chargement en VueJS
+let button = document.querySelector('input[name="addNewsBtn"]');
+bindButton(button);
+
+//remplacer le code ci-dessous par le chargement en VueJS
+let buttons = document.querySelectorAll('article button');
+buttons.forEach(function(element) {
+    element.onclick = viewdetailClick;
+});
 
 Vue.createApp({
     data() {
         return {
-            title: 'TP6 JS'
+            title: 'TP4 JS'
         }
     }
 }).mount('h1');
 
 Vue.createApp({
     data() {
+
         return {
             articles: this.getArticles(),
-
             errorMessage: null,
             titleToAdd: DEFAULTTITLE,
-            descriptionToAdd: null
+            descriptionToAdd: null,
+            colorMessage: null,
         }
     },
     computed: {
         getNbArticles() {
             return this.articles.length;
+        },
+        checkHowManyArticle() {
+            if (this.articles.length >= 1) {
+                return 'articles';
+            } else {
+                return 'article';
+            }
         }
+
     },
     methods: {
+
         getArticles() {
             return JSON.parse(ALLNEWSJSON);
         },
         viewDetailArticle(article) {
-            console.log(article.desc);
+            console.log(article.description);
+        },
+        deleteNew(article) {
+            let index = this.articles.indexOf(article);
+            this.articles.splice(index, 1);
         },
         //autre syntaxe pour déclarer une fonction
         addArticle: function(event) {
@@ -40,40 +62,21 @@ Vue.createApp({
 
             //this.titleToAdd équivalent de this.titleToAdd !== null
             if (this.titleToAdd && this.titleToAdd !== '' && this.titleToAdd !== DEFAULTTITLE) {
-                this.articles.push({ title: this.titleToAdd, desc: this.descriptionToAdd });
+                this.articles.push({ title: this.titleToAdd, description: this.descriptionToAdd });
                 this.titleToAdd = DEFAULTTITLE;
                 this.descriptionToAdd = null;
-            } else
+                this.colorMessage = 'green';
+                this.errorMessage = 'Ajout réussi !';
+            } else {
                 this.errorMessage = 'Le titre doit être renseigné';
-        }
+                this.colorMessage = 'red';
+            }
+
+        },
+
     }
 }).mount('#news');
 
-Vue.createApp({
-    data() {
-        return {
-            counter: 1
-        }
-    },
-    methods: {
-        increment() {
-            this.counter++;
-        },
-        decrement() {
-            if (this.counter > 0)
-                this.counter--;
-        }
-    }
-}).mount('#click');
-
-// Vue.createApp({})
-//     .component('ButtonCounter', ButtonCounter)
-//     .mount('#click');
-/**
- * Dans un premier temps on a counter qui vaut 1 par défaut
- * Increment va ajouter +1
- * Decrement va enlever -1
- */
 // Vue.createApp({
 //     data() {
 //         return {
@@ -82,7 +85,7 @@ Vue.createApp({
 //     },
 //     methods: {
 //         increment() {
-//             this.counter++
+//             this.counter++;
 //         },
 //         decrement() {
 //             if (this.counter > 0)
